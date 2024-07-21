792aebd0b68a4922714d757c2f614679031594b2
divyaagrawal6691 <agrawaldivya6691@gmail.com>
2024-07-21 15:27:57 +0530

    fourth commit

---
+++ Day 3 js mathematical operator/operator.js
@@ -0,0 +1,52 @@
+// Probelm 1
+let number = 6;
+let square = number ** 2;
+
+console.log(square);
+
+
+// Problem 3
+let x = 10, y = 5;
+console.log("Before Swapping: x = " + x + ", y = " + y);
+
+x = x + y;
+y = x - y;
+x = x - y;
+
+console.log("After Swapping: x = " + x + ", y = " + y);
+
+
+// Problem 4
+let c = 50;
+let f = 0;
+f = (c * 9 / 5) + 32;
+console. log("The value of the temperature in Fahrenheit is " + f);
+
+// Problem 5
+
+function isMultipleof5 (n)
+{
+    while (n > 0)
+        n = n - 5;
+ 
+    if (n == 0)
+        return true;
+ 
+    return false;
+}
+let n = 19;
+    if (isMultipleof5(n) == true)
+        console.log(n + " is multiple of 5\n");
+    else
+        console.log(n + " is not a multiple of 5\n");
+
+
+
+// Problem 6
+
+function area(length, width) {
+    var result = parseInt(length) * parseInt(width);
+    console.log("output") = result;
+}
+
+
